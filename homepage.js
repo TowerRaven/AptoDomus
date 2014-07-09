e255cf3a69aba8a159e4ca6caea2068398230151
TowerRaven <matt@towerdesigns.co.uk>
2014-07-09 14:38:15 +0100

    Added function to create edit buttons (either generally or targetted) Added function to initialise edit mode
    Signed-off-by: TowerRaven <matt@towerdesigns.co.uk>

--- homepage.js
+++ homepage.js
@@ -6,34 +6,28 @@
 $(document).ready(function() {
 	// Hide the JavaScript warning
 	$('#js-notice').addClass('hidden');
-	
+
 	// Detect local storage
 	if(!supports_html5_storage()) {
 		$('#ls-notice').removeClass('hidden');
 	} else {
 		$('#dimmer').addClass('hidden');
 	}
-	
-	/** 
-	 * initLocalStorage creates window.list of main localStorage containers 
+
+	/**
+	 * initLocalStorage creates window.list of main localStorage containers
 	 * Currently that includes:
 	 * 	window.list["rows"]
 	 * 	window.list["columns"]
 	 */
 	initLocalStorage();
-	
+
 	console.log(logUsage());
-	
-	/** 
+
+	/**
 	 * initPage generates the layout and populates it with widgets
 	 */
 	initPage('#content');
-	
-	$('#edit').on('change', function() {
-		if($(this).prop('checked') == true) {
-			console.log('Edit Mode Enabled');
-		} else {
-			console.log('Edit Mode Disabled');
-		}
-	});
+
+	initEditMode();
 });
