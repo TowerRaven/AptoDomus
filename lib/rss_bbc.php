6d1d21bd815772ad52db93b234e8ff028ee1f1b3
TowerRaven <matt@towerdesigns.co.uk>
2014-08-20 13:52:20 +0100

    Added BBC Weather XML retrieval (TESTING) RSS Address for BBC News/Weather now fixed Removed default data url for BBC News widget Adjusted read_feed to make use of an obj for other arguments
    Signed-off-by: TowerRaven <matt@towerdesigns.co.uk>

--- lib/rss_bbc.php
+++ lib/rss_bbc.php
@@ -3,7 +3,9 @@ header('Content-Type: application/json');
 
 $type = $_GET['type'];
 
+/*
 $url= urldecode($_GET['site']);
+*/
 
 function curl_get_file_contents($url) {
 	$c = curl_init();
@@ -15,19 +17,30 @@ function curl_get_file_contents($url) {
 	else return false;
 }
 
-$data = curl_get_file_contents($url);
-
 switch($type) {
 	case "news":
+		//Get data
+		$url = "http://feeds.bbci.co.uk/news/rss.xml";
+		$data = curl_get_file_contents($url);
+
 		//Parse data for json encoding
 		$simplexml = simplexml_load_string( $data );
-
 		$temp = json_decode( json_encode( $simplexml->channel) );
 
 		echo json_encode( $temp->item );
 		//echo $json;
 		break;
 	case "weather":
+		$location = $_GET['location'];
 
+		//Get data
+		$url = "http://open.live.bbc.co.uk/weather/feeds/en/".$location;
+
+		$data_obs = curl_get_file_contents($url.'/observations.rss');
+
+		$data_3day = curl_get_file_contents($url.'/3dayforecast.rss');
+
+		echo json_encode( $data_obs );
+		echo json_encode( $data_3day );
 		break;
 }
